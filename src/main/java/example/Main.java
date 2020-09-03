bab261e04d257b78b0f5658a938826a10e8e0381
zlob <zlob.jc@gmail.com>
2020-09-03 13:15:21 +0300

    #1 Read game field

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,12 +1,49 @@
 package example;
 
+import java.nio.file.*;
+import java.util.*;
+
+import lombok.*;
 import lombok.extern.slf4j.*;
 
 @Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        log.info("Hello world!!!");
+
+    }
+
+    public static char[][] readGameField(String file) {
+        List<String> lines = readFileContent(file);
+        return fillGameField(lines);
+    }
+
+    public static char[][] fillGameField(List<String> lines) {
+        int height = lines.size();
+
+        int width = 0;
+        for (String line : lines) {
+            width = Math.max(width, line.length());
+        }
+
+        char[][] gameField = new char[height][width];
+
+        int i = 0;
+        for (String line : lines) {
+            gameField[i++] = line.toCharArray();
+        }
+
+        return gameField;
+    }
+
+    @SneakyThrows
+    public static List<String> readFileContent(String file) {
+        Path filePath = Path.of(file);
+        if (!filePath.toFile().exists()) {
+            throw new RuntimeException("No file: " + file);
+        }
+
+        return Files.readAllLines(filePath);
     }
 
 }
--- src/test/java/example/MainTest.java
+++ src/test/java/example/MainTest.java
@@ -1,15 +1,41 @@
 package example;
 
+import java.util.*;
+
 import lombok.extern.slf4j.*;
 
 import org.junit.*;
 
-@Slf4j
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MainTest {
 
     @Test
-    public void foo() {
-        log.error("Test me, fix me!!!");
+    public void read_file() {
+        String file = "./src/test/resources/test-game-field-1.txt";
+        List<String> content = Main.readFileContent(file);
+        assertThat(content).isNotEmpty();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void read_file_not_present() {
+        String file = "./src/test/resources/test-game-field-666.txt";
+        List<String> content = Main.readFileContent(file);
+        fail("Must throw exception");
+    }
+
+    @Test
+    public void fill_game_field() {
+        String file = "./src/test/resources/test-game-field-1.txt";
+        List<String> content = Main.readFileContent(file);
+        char[][] gameField = Main.fillGameField(content);
+
+        boolean allLinesSameWidth = Arrays.stream(gameField)
+            .allMatch(line -> line.length == 9);
+
+        assertTrue(allLinesSameWidth);
+        assertThat(gameField.length).isEqualTo(7);
     }
 
 }
