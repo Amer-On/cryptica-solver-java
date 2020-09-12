b5174850d8c3fe02986e0b541d8df2749010231f
zlob <zlob.jc@gmail.com>
2020-09-12 14:34:52 +0300

    Move functions
    Squashed branch

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,7 +1,9 @@
 package example;
 
+import java.nio.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.*;
 
 import lombok.*;
 import lombok.extern.slf4j.*;
@@ -10,9 +12,115 @@ import lombok.extern.slf4j.*;
 public class Main {
 
     public static void main(String[] args) {
+        String file = "./src/test/resources/test-game-field-1.txt";
+    }
+
+    @Value
+    public static class Block {
+
+        private char name;
+
+        private int x;
+
+        private int y;
 
     }
 
+    //    Move blocks
+    public static char[][] move(char[][] gameField, String direction) {
+        List<Block> movableBlocks = findMovableBlocks(gameField);
+        if (direction.equals("left")) {
+            moveLeft(gameField, movableBlocks);
+        } else if (direction.equals("right")) {
+            moveRight(gameField, movableBlocks);
+        } else if (direction.equals("up")) {
+            moveUp(gameField, movableBlocks);
+        } else if (direction.equals("down")) {
+            moveDown(gameField, movableBlocks);
+        }
+        return gameField;
+    }
+
+    public static char[][] moveLeft(char[][] gameField, List<Block> movableBlocks) {
+        movableBlocks.sort(Comparator.comparingInt(Block::getY));
+        for (Block block : movableBlocks) {
+            char newCell = gameField[block.x][block.y - 1];
+            if (newCell == '-' || Character.isUpperCase(newCell)) {
+                int newY = block.y - 1;
+                gameField[block.x][newY] = block.name;
+                gameField[block.x][block.y] = newCell;
+            }
+        }
+        return gameField;
+    }
+
+    public static char[][] moveRight(char[][] gameField, List<Block> movableBlocks) {
+        movableBlocks.sort(Comparator.comparingInt(Block::getY));
+        Collections.reverse(movableBlocks);
+        for (Block block : movableBlocks) {
+            char newCell = gameField[block.x][block.y + 1];
+            if (newCell == '-' || Character.isUpperCase(newCell)) {
+                int newY = block.y + 1;
+                gameField[block.x][newY] = block.name;
+                gameField[block.x][block.y] = newCell;
+            }
+        }
+        return gameField;
+    }
+
+    public static char[][] moveDown(char[][] gameField, List<Block> movableBlocks) {
+        movableBlocks.sort(Comparator.comparingInt(Block::getX));
+        for (Block block : movableBlocks) {
+            char newCell = gameField[block.x + 1][block.y];
+            if (newCell == '-' || Character.isUpperCase(newCell)) {
+                int newX = block.x + 1;
+                gameField[newX][block.y] = block.name;
+                gameField[block.x][block.y] = newCell;
+            }
+        }
+        return gameField;
+    }
+
+    public static char[][] moveUp(char[][] gameField, List<Block> movableBlocks) {
+        movableBlocks.sort(Comparator.comparingInt(Block::getX));
+        Collections.reverse(movableBlocks);
+        for (Block block : movableBlocks) {
+            char newCell = gameField[block.x - 1][block.y];
+            if (newCell == '-' || Character.isUpperCase(newCell)) {
+                int newX = block.x - 1;
+                gameField[newX][block.y] = block.name;
+                gameField[block.x][block.y] = newCell;
+            }
+        }
+        return gameField;
+    }
+
+    public static List<Block> findMovableBlocks(char[][] gameField) {
+        List<Block> blocks = new ArrayList<Block>();
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                char cell = gameField[i][j];
+                if (cell != '-' && cell != '#' && !Character.isUpperCase(cell)) {
+                    Block block = new Block(cell, i, j);
+                    blocks.add(block);
+                }
+            }
+        }
+        return blocks;
+    }
+
+    //    Print field out
+    public static void printGameField(char[][] gameField) {
+        for (char[] row : gameField) {
+            String separator = String.valueOf(" ");
+            String joined = CharBuffer.wrap(row).chars()
+                .mapToObj(intValue -> String.valueOf((char) intValue))
+                .collect(Collectors.joining(separator));
+            System.out.println(joined);
+        }
+    }
+
+    //    Read game field
     public static char[][] readGameField(String file) {
         List<String> lines = readFileContent(file);
         return fillGameField(lines);
@@ -44,6 +152,12 @@ public class Main {
         }
 
         return Files.readAllLines(filePath);
+    }
+
+    public static String formatGameField(char[][] gameField) {
+        return Arrays.stream(gameField)
+            .map(String::valueOf)
+            .collect(Collectors.joining("\n"));
     }
 
 }
--- src/test/java/example/MainTest.java
+++ src/test/java/example/MainTest.java
@@ -2,12 +2,9 @@ package example;
 
 import java.util.*;
 
-import lombok.extern.slf4j.*;
-
 import org.junit.*;
 
-import static org.junit.Assert.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class MainTest {
 
@@ -22,7 +19,6 @@ public class MainTest {
     public void read_file_not_present() {
         String file = "./src/test/resources/test-game-field-666.txt";
         List<String> content = Main.readFileContent(file);
-        fail("Must throw exception");
     }
 
     @Test
@@ -34,8 +30,83 @@ public class MainTest {
         boolean allLinesSameWidth = Arrays.stream(gameField)
             .allMatch(line -> line.length == 9);
 
-        assertTrue(allLinesSameWidth);
+        assertThat(allLinesSameWidth).isTrue();
         assertThat(gameField.length).isEqualTo(7);
     }
 
+    @Test
+    public void find_movable_blocks() {
+        String file = "./src/test/resources/test-game-field-1.txt";
+        List<String> content = Main.readFileContent(file);
+        char[][] gameField = Main.fillGameField(content);
+
+        List<Main.Block> movableBlocks = Main.findMovableBlocks(gameField);
+        movableBlocks.forEach(System.out::println);
+    }
+
+    @Test
+    public void move_left_right() {
+        String file = "./src/test/resources/test-game-field-1.txt";
+        List<String> content = Main.readFileContent(file);
+        char[][] gameField = Main.fillGameField(content);
+        Main.printGameField(gameField);
+
+        System.out.println();
+        Main.move(gameField, "left");
+        System.out.println("Move Left:");
+        Main.printGameField(gameField);
+
+        System.out.println();
+        Main.move(gameField, "right");
+        System.out.println("Move Right:");
+        Main.printGameField(gameField);
+    }
+
+    @Test
+    public void move_down_up() {
+        String file = "./src/test/resources/test-game-field-1.txt";
+        List<String> content = Main.readFileContent(file);
+        char[][] gameField = Main.fillGameField(content);
+        Main.printGameField(gameField);
+
+        System.out.println();
+        System.out.println("Moving Left 3 times:");
+        for (int i = 0; i < 3; i++) {
+            Main.move(gameField, "left");
+        }
+        Main.printGameField(gameField);
+
+        System.out.println();
+        System.out.println("Moving Down:");
+        Main.move(gameField, "down");
+        Main.printGameField(gameField);
+
+        System.out.println();
+        System.out.println("Moving Up:");
+        Main.move(gameField, "up");
+        Main.printGameField(gameField);
+    }
+
+    @Test
+    public void move_block_through_target() {
+        List<String> content = Arrays.asList("#--Ax#");
+        char[][] gameField = Main.fillGameField(content);
+        
+        gameField = Main.move(gameField, "left");
+        String intermediateResult = Main.formatGameField(gameField);
+        assertThat(intermediateResult).isEqualTo("#--x-#");
+
+        gameField = Main.move(gameField, "left");
+        String result = Main.formatGameField(gameField);
+        assertThat(result).isEqualTo("#-xA-#");
+    }
+
+    @Test
+    public void format_multiline_game_field() {
+        List<String> content = Arrays.asList("#--#","#--#");
+        char[][] gameField = Main.fillGameField(content);
+        String result = Main.formatGameField(gameField);
+        System.out.println(result);
+        assertThat(result).isEqualTo("#--#\n#--#");
+    }
 }
